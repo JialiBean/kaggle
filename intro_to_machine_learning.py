2164f1e1561885cf3096afbad95d2609fb12b952
Jiali Chen <jialichen@MacBook-Pro-2.local>
2021-07-26 00:37:19 +0900

    Underfitting and Overfitting

--- intro_to_machine_learning.py
+++ intro_to_machine_learning.py
@@ -103,3 +103,36 @@ val_mae = mean_absolute_error(val_y, val_predictions)
 
 # uncomment following line to see the validation_mae
 print(val_mae)
+
+# 25/07/2021
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+  candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+max_leaf_dict = {}
+for max_leaf_nodes in candidate_max_leaf_nodes:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+    max_leaf_dict[max_leaf_nodes] = my_mae
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = min(max_leaf_dict, key=max_leaf_dict.get)
+print("The best tree size is " + str(best_tree_size))
+
+# Check your answer
+step_1.check()
+
+# Fill in argument to make optimal size and uncomment
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=0)
+
+# fit the final model and uncomment the next two lines
+final_model.fit(X, y)
+
+# Check your answer
+step_2.check()
