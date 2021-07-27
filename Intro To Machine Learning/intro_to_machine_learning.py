416d95ea0a7d8ce0a1dfc72a83b917ff895d1e47
JialiFish <87593724+JialiFish@users.noreply.github.com>
2021-07-28 01:57:43 +0900

    Add files via upload
    Intro To Machine Learning course

---
+++ Intro To Machine Learning/intro_to_machine_learning.py
@@ -0,0 +1,231 @@
+# 2021/07/22
+
+# Basic data exploration
+
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
+
+# Print summary statistics in next line
+home_data.describe()
+
+# What is the average lot size (rounded to nearest integer)?
+avg_lot_size = 10517
+
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age = 11
+
+# 2021/07/23
+# Your first machine learning model
+
+# print the list of columns in the dataset to find the name of the prediction target
+home_data.columns
+
+y = home_data.SalePrice
+
+# Create the list of features below
+feature_names = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+
+# Select data corresponding to features in feature_names
+X = home_data[feature_names]
+
+# Review data
+# print description or statistics from X
+print(X.describe())
+
+# print the top few lines
+print(X.head())
+
+from sklearn.tree import DecisionTreeRegressor
+
+# specify the model.
+# For model reproducibility, set a numeric value for random_state when specifying the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit the model
+iowa_model.fit(X, y)
+
+predictions = iowa_model.predict(X)
+print(predictions)
+
+print("Making predictions for the first values:")
+print(X.head())
+print("The predictions are")
+print(iowa_model.predict(X.head()))
+print("y first values are:")
+print(y.head())
+
+# Model validation
+
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[feature_columns]
+
+# Specify Model
+iowa_model = DecisionTreeRegressor()
+# Fit Model
+iowa_model.fit(X, y)
+
+print("First in-sample predictions:", iowa_model.predict(X.head()))
+print("Actual target values for those homes:", y.head().tolist())
+
+# Import the train_test_split function and uncomment
+from sklearn.model_selection import train_test_split
+
+# fill in and uncomment
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# You imported DecisionTreeRegressor in your last exercise
+# and that code has been copied to the setup code above. So, no need to
+# import it again
+
+# Specify the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit iowa_model with the training data.
+iowa_model.fit(train_X, train_y)
+
+# Predict with all validation observations
+val_predictions = iowa_model.predict(val_X)
+
+from sklearn.metrics import mean_absolute_error
+
+val_mae = mean_absolute_error(val_y, val_predictions)
+
+# uncomment following line to see the validation_mae
+print(val_mae)
+
+
+# 25/07/2021
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return (mae)
+
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+max_leaf_dict = {}
+for max_leaf_nodes in candidate_max_leaf_nodes:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" % (max_leaf_nodes, my_mae))
+    max_leaf_dict[max_leaf_nodes] = my_mae
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = min(max_leaf_dict, key=max_leaf_dict.get)
+print("The best tree size is " + str(best_tree_size))
+
+# Check your answer
+step_1.check()
+
+# Fill in argument to make optimal size and uncomment
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=0)
+
+# fit the final model and uncomment the next two lines
+final_model.fit(X, y)
+
+# Check your answer
+step_2.check()
+
+# 27/07/2021
+
+from sklearn.ensemble import RandomForestRegressor
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+val_preds = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(val_y, val_preds)
+
+print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+
+# Check your answer
+step_1.check()
+
+# Machine Learning Competitions
+
+# Set up code checking
+from learntools.core import binder
+
+binder.bind(globals())
+from learntools.machine_learning.ex7 import *
+
+# Set up filepaths
+import os
+
+if not os.path.exists("../input/train.csv"):
+    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")
+    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv")
+
+# Import helpful libraries
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+# Load the data, and separate the target
+iowa_file_path = '../input/train.csv'
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+
+# Create X (After completing the exercise, you can return to modify this line!)
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+
+# Select columns corresponding to features, and preview the data
+X = home_data[features]
+X.head()
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+# To improve accuracy, create a new Random Forest model which you will train on all training data
+rf_model_on_full_data = RandomForestRegressor(random_state=1)
+
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(X, y)
+
+# path to file you will use for predictions
+test_data_path = '../input/test.csv'
+
+# read test data file using pandas
+test_data = pd.read_csv(test_data_path)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = test_data[features]
+
+# make predictions which we will submit.
+test_preds = rf_model_on_full_data.predict(test_X)
+print(test_preds)
+
+# Run the code to save predictions in the format used for competition scoring
+
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': test_preds})
+output.to_csv('submission.csv', index=False)
